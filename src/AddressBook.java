2ef3b82c1dae97f106b9477d5fda095df37813ff
Patrick Liu <patrickliu@cmail.carleton.ca>
2021-09-16 22:23:56 -0400

    Second commit

---
+++ src/AddressBook.java
@@ -0,0 +1,64 @@
+/* Yuguo Liu */
+/* 101142730 */
+
+import java.util.ArrayList;
+
+public class AddressBook {
+
+    private ArrayList<BuddyInfo> buddyList = new ArrayList<>();
+
+    public void addBuddy(BuddyInfo buddy){
+
+        boolean added = false;
+
+        if(buddyList.isEmpty()){
+            buddyList.add(buddy);
+        }
+        else{
+            // avoid duplicate buddyInfo objects
+            for (BuddyInfo num : buddyList) {
+                if(num.equals(buddy)){
+                    added = true;
+                    break;
+                }
+            }
+
+            if(!added){
+                buddyList.add(buddy);
+            }
+        }
+    }
+
+    public void removeBuddy(BuddyInfo buddy){
+        buddyList.removeIf(num -> num.equals(buddy));
+    }
+
+    public static void main(String args[]){
+        BuddyInfo buddy1 = new BuddyInfo("Patrick", "Carleton", "613");
+        BuddyInfo buddy2 = new BuddyInfo("Jack", "Kanata", "611");
+        AddressBook addressBook = new AddressBook();
+
+        //Duplicate objects will not be added to the Arraylist
+        addressBook.addBuddy(buddy1);
+        addressBook.addBuddy(buddy1);
+
+        //New objects should be added
+        addressBook.addBuddy(buddy2);
+
+        //Checking the content of the Arraylist
+        for (BuddyInfo num : addressBook.buddyList) {
+            System.out.println(num.getName() + " " + num.getAddress() + " " + num.getPhoneNumber());
+        }
+
+        //Remove buddy1
+        addressBook.removeBuddy(buddy1);
+
+        //Removing an object that no longer exists in the Arraylist should not produce an error
+        addressBook.removeBuddy(buddy1);
+
+        //Checking the content of the Arraylist
+        for (BuddyInfo num : addressBook.buddyList) {
+            System.out.println("\nAfter removing buddy1:\n" + num.getName() + " " + num.getAddress() + " " + num.getPhoneNumber());
+        }
+    }
+}
